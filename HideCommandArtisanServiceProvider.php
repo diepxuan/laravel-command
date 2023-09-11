5500b30c8f3d52b4dbc5756042655211c5c20f45
Tran Ngoc Duc <caothu91@gmail.com>
2023-09-11 07:19:50 +0700

    call providers from variable $providers

--- HideCommandArtisanServiceProvider.php
+++ HideCommandArtisanServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Diepxuan\HideCommand;
 
-use Illuminate\Database\MigrationServiceProvider as IlluminateProvider;
+use Illuminate\Foundation\Providers\ArtisanServiceProvider as IlluminateProvider;
 use Symfony\Component\Console\Command\Command;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
@@ -103,20 +103,7 @@ use Illuminate\Routing\Console\ControllerMakeCommand;
 use Illuminate\Routing\Console\MiddlewareMakeCommand;
 use Illuminate\Session\Console\SessionTableCommand;
 
-use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Database\Console\Migrations\FreshCommand;
-use Illuminate\Database\Console\Migrations\InstallCommand;
-use Illuminate\Database\Console\Migrations\MigrateCommand;
-use Illuminate\Database\Console\Migrations\MigrateMakeCommand;
-use Illuminate\Database\Console\Migrations\RefreshCommand;
-use Illuminate\Database\Console\Migrations\ResetCommand;
-use Illuminate\Database\Console\Migrations\RollbackCommand;
-use Illuminate\Database\Console\Migrations\StatusCommand;
-use Illuminate\Database\Migrations\DatabaseMigrationRepository;
-use Illuminate\Database\Migrations\MigrationCreator;
-use Illuminate\Database\Migrations\Migrator;
-
-class MigrationServiceProvider extends IlluminateProvider
+class HideCommandArtisanServiceProvider extends IlluminateProvider
 {
     /**
      * The commands to be registered.
@@ -124,87 +111,110 @@ class MigrationServiceProvider extends IlluminateProvider
      * @var array
      */
     protected $commands = [
-        'Migrate' => MigrateCommand::class,
-        'MigrateFresh' => FreshCommand::class,
-        'MigrateInstall' => InstallCommand::class,
-        'MigrateRefresh' => RefreshCommand::class,
-        'MigrateReset' => ResetCommand::class,
-        'MigrateRollback' => RollbackCommand::class,
-        'MigrateStatus' => StatusCommand::class,
-        'MigrateMake' => MigrateMakeCommand::class,
+        // 'About' => AboutCommand::class,
+        // 'CacheClear' => CacheClearCommand::class,
+        // 'CacheForget' => CacheForgetCommand::class,
+        // 'ClearCompiled' => ClearCompiledCommand::class,
+        // 'ClearResets' => ClearResetsCommand::class,
+        // 'ConfigCache' => ConfigCacheCommand::class,
+        // 'ConfigClear' => ConfigClearCommand::class,
+        // 'ConfigShow' => ConfigShowCommand::class,
+        // 'Db' => DbCommand::class,
+        // 'DbMonitor' => DatabaseMonitorCommand::class,
+        // 'DbPrune' => PruneCommand::class,
+        // 'DbShow' => ShowCommand::class,
+        // 'DbTable' => DatabaseTableCommand::class,
+        // 'DbWipe' => WipeCommand::class,
+        // 'Down' => DownCommand::class,
+        // 'Environment' => EnvironmentCommand::class,
+        // 'EnvironmentDecrypt' => EnvironmentDecryptCommand::class,
+        // 'EnvironmentEncrypt' => EnvironmentEncryptCommand::class,
+        // 'EventCache' => EventCacheCommand::class,
+        // 'EventClear' => EventClearCommand::class,
+        // 'EventList' => EventListCommand::class,
+        // 'KeyGenerate' => KeyGenerateCommand::class,
+        // 'Optimize' => OptimizeCommand::class,
+        // 'OptimizeClear' => OptimizeClearCommand::class,
+        // 'PackageDiscover' => PackageDiscoverCommand::class,
+        // 'PruneStaleTagsCommand' => PruneStaleTagsCommand::class,
+        // 'QueueClear' => QueueClearCommand::class,
+        // 'QueueFailed' => ListFailedQueueCommand::class,
+        // 'QueueFlush' => FlushFailedQueueCommand::class,
+        // 'QueueForget' => ForgetFailedQueueCommand::class,
+        // 'QueueListen' => QueueListenCommand::class,
+        // 'QueueMonitor' => QueueMonitorCommand::class,
+        // 'QueuePruneBatches' => QueuePruneBatchesCommand::class,
+        // 'QueuePruneFailedJobs' => QueuePruneFailedJobsCommand::class,
+        // 'QueueRestart' => QueueRestartCommand::class,
+        // 'QueueRetry' => QueueRetryCommand::class,
+        // 'QueueRetryBatch' => QueueRetryBatchCommand::class,
+        // 'QueueWork' => QueueWorkCommand::class,
+        // 'RouteCache' => RouteCacheCommand::class,
+        // 'RouteClear' => RouteClearCommand::class,
+        // 'RouteList' => RouteListCommand::class,
+        // 'SchemaDump' => DumpCommand::class,
+        // 'Seed' => SeedCommand::class,
+        'ScheduleFinish' => ScheduleFinishCommand::class,
+        'ScheduleList' => ScheduleListCommand::class,
+        'ScheduleRun' => ScheduleRunCommand::class,
+        'ScheduleClearCache' => ScheduleClearCacheCommand::class,
+        'ScheduleTest' => ScheduleTestCommand::class,
+        'ScheduleWork' => ScheduleWorkCommand::class,
+        'ScheduleInterrupt' => ScheduleInterruptCommand::class,
+        // 'ShowModel' => ShowModelCommand::class,
+        // 'StorageLink' => StorageLinkCommand::class,
+        // 'Up' => UpCommand::class,
+        // 'ViewCache' => ViewCacheCommand::class,
+        // 'ViewClear' => ViewClearCommand::class,
     ];
 
-    private static $laravelDefaultCommands = [
-        'about',
-        'clear-compiled',
-        'db',
-        'docs',
-        'down',
-        'env',
-        'help',
-        'inspire',
-        // 'list',
-        'migrate',
-        'optimize',
-        'serve',
-        // 'completion',
-        'tinker',
-        'up',
-        'auth:clear-resets',
-        'cache:*',
-        'channel:list',
-        'config:*',
-        'db:*',
-        'debugbar:clear',
-        'env:*',
-        'event:*',
-        'key:generate',
-        'lang:publish',
-        'make:*',
-        'migrate:*',
-        'model:*',
-        'notifications:table',
-        'optimize:clear',
-        'package:discover',
-        'queue:*',
-        'route:*',
-        'schema:dump',
-        'session:table',
-        'storage:link',
-        'stub:publish',
-        'vendor:publish',
-        'view:*',
+    /**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $devCommands = [
+        // 'CacheTable' => CacheTableCommand::class,
+        // 'CastMake' => CastMakeCommand::class,
+        // 'ChannelList' => ChannelListCommand::class,
+        // 'ChannelMake' => ChannelMakeCommand::class,
+        // 'ComponentMake' => ComponentMakeCommand::class,
+        // 'ConsoleMake' => ConsoleMakeCommand::class,
+        // 'ControllerMake' => ControllerMakeCommand::class,
+        // 'Docs' => DocsCommand::class,
+        // 'EventGenerate' => EventGenerateCommand::class,
+        // 'EventMake' => EventMakeCommand::class,
+        // 'ExceptionMake' => ExceptionMakeCommand::class,
+        // 'FactoryMake' => FactoryMakeCommand::class,
+        // 'JobMake' => JobMakeCommand::class,
+        // 'LangPublish' => LangPublishCommand::class,
+        // 'ListenerMake' => ListenerMakeCommand::class,
+        // 'MailMake' => MailMakeCommand::class,
+        // 'MiddlewareMake' => MiddlewareMakeCommand::class,
+        // 'ModelMake' => ModelMakeCommand::class,
+        // 'NotificationMake' => NotificationMakeCommand::class,
+        // 'NotificationTable' => NotificationTableCommand::class,
+        // 'ObserverMake' => ObserverMakeCommand::class,
+        // 'PolicyMake' => PolicyMakeCommand::class,
+        // 'ProviderMake' => ProviderMakeCommand::class,
+        // 'QueueFailedTable' => FailedTableCommand::class,
+        // 'QueueTable' => TableCommand::class,
+        // 'QueueBatchesTable' => BatchesTableCommand::class,
+        // 'RequestMake' => RequestMakeCommand::class,
+        // 'ResourceMake' => ResourceMakeCommand::class,
+        // 'RuleMake' => RuleMakeCommand::class,
+        // 'ScopeMake' => ScopeMakeCommand::class,
+        // 'SeederMake' => SeederMakeCommand::class,
+        // 'SessionTable' => SessionTableCommand::class,
+        // 'Serve' => ServeCommand::class,
+        // 'StubPublish' => StubPublishCommand::class,
+        // 'TestMake' => TestMakeCommand::class,
+        // 'VendorPublish' => VendorPublishCommand::class,
+        // 'ViewMake' => ViewMakeCommand::class,
     ];
 
     public function boot()
     {
-        $this->app->booted(function () {
-            $hideCommands = collect(Artisan::all())
-                ->filter(function (Command $command) {
-                    return !$command->isHidden();
-                })
-                ->filter(function (Command $command) {
-                    return $command->isEnabled();
-                })
-                ->filter(function (Command $command) {
-                    // foreach (self::$laravelDefaultCommands as $cmd) {
-                    //     if (Str::of($command->getName())->is($cmd))
-                    //         return true;
-                    // }
-                    return false;
-                })
-                // ->only(self::$laravelDefaultCommands)
-                ->map(function (Command $command) {
-                    $command->setHidden(true);
-                    return $command;
-                })
-                ->toArray();
-            // app()->
-            $this->commands($hideCommands);
-        });
-
-        $this->commands([
-            \Diepxuan\HideCommand\HideCommand::class,
-        ]);
+        //
     }
 }
